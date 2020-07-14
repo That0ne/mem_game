4549ebcb48ee4b4aa12ea898e570117f581efefd
That0ne <brent.j.franklin@gmail.com>
2020-07-13 18:52:22 -0700

    finalized features

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const gameContainer = document.getElementById("game");
 const resetButton = document.getElementById('reset');
 let container  = document.getElementById('container');
-let counter = document.querySelector('#counter');
 let addMore = document.querySelector('#addMore');
+let lowScore = document.querySelector('h3');
 let count = 1;
 let matchArr = [];
 
-const COLORS = [
+// set initial low score to 0 if localStorage is null
+let LS = localStorage.getItem('Low Score');
+if(LS === null){
+  lowScore.innerText = 'Low Score = 0'
+}else {
+lowScore.innerText = `Low Score = ${LS}`;
+};
+
+
+let COLORS = [
   "red",
   "blue",
   "green",
   "orange",
   "purple",
+  "yellow",
   "red",
   "blue",
   "green",
   "orange",
-  "purple"
+  "purple",
+  "yellow"
 ];
 
 //randomizing the color array
@@ -31,37 +42,41 @@ function shuffle(array) {
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
 //enabling the start button
-counter.addEventListener("click", createDivsForColors);
+addMore.addEventListener("click", createDivsForColors);
 
 
-// Creating divs for cards and adding classes
+// creating divs for cards and adding classes
 function createDivsForColors() {
   let shuffledColors = shuffle(COLORS);
   for (let color of shuffledColors) {
     const newDiv = document.createElement("div");   
     newDiv.classList.add(color);
-
     //adds event listener to click card and change color
     newDiv.addEventListener("click", currentCard);
-
     gameContainer.append(newDiv);   
   }
   //disabling the start button
-  counter.removeEventListener('click', createDivsForColors);
-
+  addMore.removeEventListener('click', createDivsForColors);
   //enabling the add cards button
   addMore.addEventListener('click', addMoreCards);
+  addMore.innerText = "ADD MORE CARDS"
 }
+
+
+// once game has started disable add cards
+gameContainer.addEventListener("click", () => {
+  addMore.removeEventListener('click', createDivsForColors);
+  addMore.removeEventListener('click', addMoreCards);
+});
 
 
 
 // adds 4 more cards with new colors added to color array
-function addMoreCards(e){
+function addMoreCards(){
   let newCards = 0;
   while(newCards <2){
     const randomColor = 'a' + Math.random().toString(16).slice(2, 8);
@@ -77,27 +92,47 @@ function addMoreCards(e){
 
 
 //resets the entire game
-resetButton.addEventListener('click', function timedRefresh(time) {
-	setTimeout("location.reload(true);", 0);
+resetButton.addEventListener('click', function () {
+  gameContainer.innerHTML = "";
+  count = 1;
+  COLORS = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "yellow",
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "yellow"
+  ];;
+  addMore.innerText = "START";
+  addMore.addEventListener('click', createDivsForColors);
+  lowScore.innerText = `Low Score = ${localStorage.getItem('Low Score')}`;
 })
 
 
 
-//
+//function for logic of matching card game
 function currentCard(e) {
   //once clicked it removes the event listener so it cant be clicked again
   e.target.removeEventListener("click", currentCard);
   let removeAll = document.querySelectorAll('div > div');
   let remove = document.querySelectorAll(`div[class = ${e.target.classList[0]}]`);
   //the math to increment the counter
-  counter.innerText = Math.floor((count+1)/2);
+  let turn = Math.floor((count+1)/2);
+  addMore.innerText = turn;
   
   //change color of card depending on the class attribute
   if(e.target.classList.value === 'red' || 
     e.target.classList.value === 'blue' || 
     e.target.classList.value ==='green' || 
     e.target.classList.value ==='purple' || 
-    e.target.classList.value ==='orange'){
+    e.target.classList.value ==='orange' ||
+    e.target.classList.value ==='yellow'){
     e.target.style.backgroundColor = e.target.classList.value;
   } else{
     let color = e.target.classList.value;
@@ -106,34 +141,25 @@ function currentCard(e) {
     colorString = colorArr.join('');
     e.target.style.backgroundColor = `#${colorString}`;
   };
-
-  for(let i = 0; i <removeAll.length; i++){
-    if(!removeAll[i].backgroundColor){
-      break;
-    }else{
-      localStorage.setItem('Low Score', counter.innerText);
-    }
-  }
-
+  
   //for matching the two cards during each attempt
   matchArr.push(e.target);
   
 
   //if they match, remove event listener and keep color
   if(count%2 === 0 && e.target.classList[0] === matchArr[matchArr.length-2].classList[0]){
-    counter.innerText = `HOORAY!`;
+    addMore.innerText = `HOORAY!`;
     setTimeout(function(){
-      counter.innerText = Math.floor((count+1)/2);
+      addMore.innerText = turn;
     },1000);
     remove[0].removeEventListener("click", currentCard);
     remove[1].removeEventListener("click", currentCard);
     
     //if they dont match try again
   } else if (count%2 === 0 && e.currentTarget.classList[0] !== matchArr[matchArr.length-2].classList[0]){
-    counter.innerText = `Try Again`;
+      addMore.innerText = `Try Again`;
     setTimeout(function(){
-      counter.innerText = Math.floor((count+1)/2);
-      // set localStorage with low score if all divs have background colors
+        addMore.innerText = turn;
     },1000);
     removeAll.forEach(item => {
       item.removeEventListener('click', currentCard);
@@ -141,25 +167,28 @@ function currentCard(e) {
     
     //if they dont match change back to white and increment counter
     setTimeout(function (){
-      e.target.style.backgroundColor = '';
-      matchArr[matchArr.length-2].style.backgroundColor = '';
+      e.target.removeAttribute('style');
+      matchArr[matchArr.length-2].removeAttribute('style');
       removeAll.forEach(item => {
         item.addEventListener('click', currentCard);
       })
       
     }, 1000);
   }
-  count++
+
+
+  // set low score in localStorage if new low score or no low score
+  let cardList = Array.from(removeAll);
   
-  
-  
+  if(cardList.every(item => item.hasAttribute('style')) && 
+    (turn < localStorage.getItem('Low Score') || 
+    localStorage.getItem('Low Score') === null)){
+    
+      localStorage.setItem('Low Score', turn);
+  };
+
+  count++ 
 };
 
-// for(let i = 0; i <removeAll.length; i++){
-//   if(!removeAll[i].backgroundColor){
-//     break;
-//   }else{
-//     localStorage.setItem('Low Score', counter.innerText);
-//   }
-// }
+
 
